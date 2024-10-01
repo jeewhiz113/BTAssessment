a5db6deb1cd7fc94766310000130ac46537036dd
learndeployment <learndeployment@gmail.com>
2024-09-30 19:49:12 -0500

    Added UI

---
+++ AttractionsUI/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace AttractionsUI
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new MainPage();
+        }
+    }
+}
