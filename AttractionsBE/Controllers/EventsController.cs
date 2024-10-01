e0595bf2b28ba888881a2f1529b75be187e8993a
learndeployment <learndeployment@gmail.com>
2024-09-30 19:39:17 -0500

    Added Dependency Injection and appsettings values

---
+++ AttractionsBE/Controllers/EventsController.cs
@@ -0,0 +1,28 @@
+﻿using AttractionsBE.Models;
+using AttractionsBE.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace AttractionsBE.Controllers
+{
+    [ApiController]
+    [Route("api/events")]
+    public class EventsController : ControllerBase
+    {
+        private readonly ITicketMasterApiService _ticketMasterApiService;
+        public EventsController(ITicketMasterApiService ticketMasterApiService)
+        {
+            _ticketMasterApiService = ticketMasterApiService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<TicketMasterEventsDto?>> GetEvents([FromQuery] string attractionId)
+        {
+            var AttractionResult = await _ticketMasterApiService.FindEventsAsync(attractionId);
+            {
+                return Ok(AttractionResult);
+
+            }
+        }
+    }
+    
+}
